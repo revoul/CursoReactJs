4e891a6e7dee891bb1b35f97581bf9188fba6397
Revoul <lordizmur@gmail.com>
2015-06-18 17:24:58 +0200

    Se modula la aplicación

---
+++ Clase#2/app.js
@@ -0,0 +1,16 @@
+/*
+* Module dependencies
+*/
+
+import React from 'react';
+import MyTabla form './components/MyTabla';
+
+var myArray = [
+	{number: 1, name: 'Primero'},
+	{number: 2, name: 'Segundo'},
+	{number: 3, name: 'Tercero'}
+];
+
+React.render(
+	<MyTabla objetos={myArray} />, document.getElementById('container')
+ );
